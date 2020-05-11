725c3c5f3ab3e2e1590d1075e79a7d838e0e63c3
Viktor Meyer <viktorme@kth.se>
2020-05-11 12:17:36 +0200

    A2

---
+++ Assignment 2/Exercise 2/cmake-build-debug/CMakeFiles/Exercise_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_2.dir/stream.cpp.o"
+  "Exercise_2"
+  "Exercise_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
