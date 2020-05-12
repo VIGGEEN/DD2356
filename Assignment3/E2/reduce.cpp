bb8efa730142534d8ab93a12cfb2cff71bd66596
Viktor Meyer <viktorme@kth.se>
2020-05-12 20:12:01 +0200

    A3E2.6

--- Assignment3/E2/reduce.cpp
+++ Assignment3/E2/reduce.cpp
@@ -24,6 +24,10 @@ int main(int argc, char **argv) {
     std::default_random_engine generator(seed);
     std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
+    int* bptr;
+    MPI_Win wptr;
+    MPI_Win_allocate(world_size*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &bptr,&wptr);
+
     // Calculate PI following a Monte Carlo method
     for (size_t iter = 0; iter < (NUM_ITER / world_size); iter++) {
         // Generate random (X,Y) points
@@ -33,14 +37,17 @@ int main(int argc, char **argv) {
         if ((x * x) + (y * y) <= 1.0) count++;
     }
 
-    int reducebuffer;
-    MPI_Reduce(&count, &reducebuffer, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Put(&count, 1, MPI_INT, 0, world_rank, 1, MPI_INT, wptr);
+    MPI_Win_fence(0, wptr);
+
     if (world_rank == 0) {
-        count = reducebuffer;
+        for(int i = 1; i < world_size; i++) count += *(bptr+i);
         // Estimate Pi and display the result
         const auto pi = ((double) count / (double) NUM_ITER) * 4.0;
         printf("The result is %f, computed in %f\n", pi, MPI_Wtime() - start);
     }
+
+    MPI_Win_free(&wptr);
 
     MPI_Finalize();
 
