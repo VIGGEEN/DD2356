385dbeb6a6b74c7e725e24b16bdc33cea1aeb33c
Viktor Meyer <viktorme@kth.se>
2020-05-12 22:03:18 +0200

    A3E3

---
+++ Assignment3/E3/cmake-build-release/CMakeFiles/E3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E3.dir/ping_pong.cpp.o"
+  "E3"
+  "E3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
